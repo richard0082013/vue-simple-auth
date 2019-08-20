49144fb9b0f230c7d024895dd318305221858902
Bo Tian <richard0082013@gmail.com>
2019-08-20 15:54:15 +1000

    hide firebase config

--- src/main.js
+++ src/main.js
@@ -6,15 +6,7 @@ import router from "./router";
 Vue.config.productionTip = false;
 let app = "";
 // Your web app's Firebase configuration
-const firebaseConfig = {
-  apiKey: "AIzaSyAw9oc7nmYvbVHJcBHV2MHVuIz0Pqh0XYs",
-  authDomain: "vue-firebase-008.firebaseapp.com",
-  databaseURL: "https://vue-firebase-008.firebaseio.com",
-  projectId: "vue-firebase-008",
-  storageBucket: "",
-  messagingSenderId: "193778379393",
-  appId: "1:193778379393:web:e623e280615ee42d"
-};
+const firebaseConfig = {};
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 firebase.auth().onAuthStateChanged(() => {
